51952b19ea0647a5c1e07c03d36bc9022b8587fe
Anthony Johnson <you@example.com>
2023-09-26 18:56:47 +0000

    Cleans data by removing NA values from data.

--- johnson.pkg/R/alligators_NAs.R
+++ johnson.pkg/R/alligators_NAs.R
@@ -1,6 +1,12 @@
+# installing packages and loading data
+
 install.packages("tidyverse")
 library(tidyverse)
+
+#loading data from CSV
 Alligators <- read_csv("johnson.pkg/R/AlligatorPracticeData.csv")
+
+#cleaning data by removing NAs
 
 alligators_NAs <- Alligators %>% 
   na.omit()
