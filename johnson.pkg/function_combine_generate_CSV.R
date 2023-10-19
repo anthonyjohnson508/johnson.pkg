4aa6bffc8db0de331d1f7d51986e6d2ffc061255
Anthony Johnson <you@example.com>
2023-10-19 17:05:08 +0000

    #Function for the final project. This function will be used to combine all data manipulations performed and output an excel spreadsheat (.csv). This function will be useful as it will allow me to create a new spreadsheet that can be used to make maps in ArcGIS. This would be useful for future data exploration and visualizations for publication. This function will combine data from function_reclassify and function_Kmeans_Cluster.

---
+++ johnson.pkg/function_combine_generate_CSV.R
@@ -0,0 +1,8 @@
+#Function for the final project. 
+#This function will be used to combine all data manipulations performed and output an excel spreadsheat (.csv).
+#This function will be useful as it will allow me to create a new spreadsheet that can be used to make maps in ArcGIS.
+#This would be useful for future data exploration and visualizations for publication.
+#This function will combine data from function_reclassify and function_Kmeans_Cluster.
+
+
+#add function here....
