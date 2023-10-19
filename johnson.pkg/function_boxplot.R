bcafc315a060d692322d51558438fcbeec2f517b
Anthony Johnson <you@example.com>
2023-10-19 16:49:36 +0000

    Function for final project. This function plot results from linear regression (see function_LM_HL_class.r). This function will likely be represented in a box whisker plot (however I may experiment with a Cleveland plot although I may not have enough data, I could possibly reduce the number of cluster groups).

---
+++ johnson.pkg/function_boxplot.R
@@ -0,0 +1,6 @@
+#function for final project
+#this function plot results from linear regression (see function_LM_HL_class.r)
+#this function will likely be represented in a box whisker plot (however I may experiement with a Cleavland plot although I may not have enough data, I could possibly reduce the number of cluster groups)
+
+
+#add function here...
