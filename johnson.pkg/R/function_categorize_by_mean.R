6d4709e4b18968d0c3a494c3b100acf77375200a
Anthony Johnson <you@example.com>
2023-10-26 16:53:40 +0000

    Adjusted spacing for document information. Just for aestetics.

--- johnson.pkg/R/function_categorize_by_mean.R
+++ johnson.pkg/R/function_categorize_by_mean.R
@@ -3,6 +3,7 @@
 #'this creates a new column titled Size and gives the observation a value of Small or Large depending on if the observation is great or less than the mean. 
 #'
 #'
+#'
 #'@param data the data set being used, should be .csv
 #'@param column_name this is the column that you wish to peform the analysis on. This should be your total length column. 
 #'@return a mutated data set that adds an additional column Size, under the new column each observation will receive a value Large or Small.
